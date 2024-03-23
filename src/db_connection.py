1fab1fe9ae864eb125677982b25935bb8a052109
Max <ibroden@yandex.ru>
2024-03-23 14:31:04 +0300

    db connection variables were changed

--- src/db_connection.py
+++ src/db_connection.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 #  stashed postgres password as OS variable ('dialect+driver://username:password@host:port/database')
-SQLALCHEMY_DATABASE_URL = f'postgresql://{os.environ["POSTGRES_USR"]}:{os.environ["POSTGRES_PSW"]}' \
-                          f'@{os.environ["POSTGRES_HOST"]}:{os.environ["POSTGRES_PORT"]}/{os.environ["POSTGRES_DB_NAME"]}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{os.environ["POSTGRES_USER"]}:{os.environ["POSTGRES_PASSWORD"]}' \
+                          f'@{os.environ["POSTGRES_HOST"]}:{os.environ["POSTGRES_PORT"]}/{os.environ["POSTGRES_DB"]}'
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
